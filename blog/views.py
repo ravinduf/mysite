734601d0b134acb31c89c946e9805a48004b9e2d
raavinduf <ravinduf@rocketmail.com>
2020-03-02 08:16:37 +0530

    added django tags

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.filter().order_by('created_date')
+    return render(request,'blog/post_list.html',{'posts':posts})
     
