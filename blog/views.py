a2cd8ca2dba156e552504442436c5f453ffc3645
raavinduf <ravinduf@rocketmail.com>
2020-03-06 09:09:16 +0530

    Added views to create/edit blog post inside the site

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.shortcuts import redirect
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.utils import timezone
-#from django.shortcuts import render,get_object_or_404
+from .forms import PostForm
+
 
 # Create your views here.
 
@@ -13,3 +15,29 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.created_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
