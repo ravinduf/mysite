6d46bab81d7647df9f465d126ab03d42ae7ad802
raavinduf <ravinduf@rocketmail.com>
2020-03-23 22:36:17 +0530

    updated settings and wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,5 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
